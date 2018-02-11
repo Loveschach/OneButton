4d3ee64c72afada8187e313699724112fb2d23f9
Cody Malach <codymalach@gmail.com>
2018-02-11 14:33:01 -0800

    Not working jump

--- One Button/Assets/Scripts/Controller.cs
+++ One Button/Assets/Scripts/Controller.cs
@@ -7,24 +7,36 @@ public class Controller : MonoBehaviour {
 	const float TOP_SPEED = 4.5f;
 	const float SPEED_DAMPING = 0.25f;
 	const float MAX_JUMP_TIME = 1f;
+	const float JUMP_FORCE = 3f;
 	Rigidbody2D body;
-	float jumpStartTime = -1;
+	BoxCollider2D boxCollider;
+	LayerMask groundLayer;
 
 	// Use this for initialization
 	void Start () {
 		body = GetComponent<Rigidbody2D>();
+		boxCollider = GetComponent<BoxCollider2D>();
+		groundLayer = LayerMask.GetMask( "Ground" );
 	}
 	
+	bool IsGrounded() {
+		Vector3 lineEnd = new Vector3( boxCollider.bounds.center.x, boxCollider.bounds.min.y - 0.5f, boxCollider.bounds.center.z );
+		return Physics.Linecast( boxCollider.bounds.center, lineEnd, groundLayer );
+	}
+
 	void FixedUpdate () {
 		float xVelocity = Mathf.Lerp( body.velocity.x, TOP_SPEED * Input.GetAxisRaw( "Horizontal" ), SPEED_DAMPING );
 		body.velocity = new Vector2( xVelocity, body.velocity.y );
+		Vector3 capsuleBounds = new Vector3( boxCollider.bounds.center.x, boxCollider.bounds.min.y - 0.5f, boxCollider.bounds.center.z );
+		Debug.DrawLine( boxCollider.bounds.center, capsuleBounds );
 
 		float yVelocity = body.velocity.y;
-		if( Input.GetKeyDown( KeyCode.Jump) ) {
-			jumpStartTime = jumpStartTime == -1 ? Time.time : jumpStartTime;
+		if( IsGrounded() ) {
+			Debug.Log( "grounded" );
 		}
-		else {
-			jumpStartTime = -1;
+		if( Input.GetButton( "Jump" ) && IsGrounded() ) {
+			Debug.Log( "jump" );
+			body.AddForce( new Vector2( 0, JUMP_FORCE ) );
 		}
 	}
 }
