3a7183631b820c7f0f48134695d38fdc316eadbf
Cody Malach <codymalach@gmail.com>
2018-02-18 13:33:33 -0800

    Remove git ignore files. Move actions into separate utils.

---
+++ One Button/Assets/Scripts/Player/Actions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Actions {
+	public enum ButtonActions {
+		JUMP,
+		SLIDE,
+		FLAP
+	};
+	const float JUMP_FORCE = 100f;
+
+	public static void ExecuteCurrentAction( Controller controller ) {
+		ButtonActions currentAction = controller.GetCurrentAction();
+		switch( currentAction ) {
+			case ButtonActions.JUMP:
+				ExecuteJumpAction( controller );
+				break;
+			case ButtonActions.SLIDE:
+				break;
+			case ButtonActions.FLAP:
+				break;
+			default:
+				Debug.Assert( false, "Attempting to get force for non-existent action." );
+				break;
+		}
+	}
+
+	public static void ExecuteJumpAction( Controller controller ) {
+		if( controller.IsGrounded() ) {
+			controller.GetBody().AddForce( new Vector2( 0, JUMP_FORCE ) );
+		}
+	}
+}
---
+++ One Button/Assets/Scripts/Player/Controller.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+	const float TOP_SPEED = 4.5f;
+	const float SPEED_DAMPING = 0.25f;
+	const float GROUND_RAY_LENGTH = 0.25f;
+	Rigidbody2D body;
+	BoxCollider2D boxCollider;
+	LayerMask groundLayer;
+	Actions.ButtonActions currentAction;
+
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody2D>();
+		boxCollider = GetComponent<BoxCollider2D>();
+		groundLayer = LayerMask.GetMask( "Ground" );
+	}
+
+	public Actions.ButtonActions GetCurrentAction() {
+		return currentAction;
+	}
+
+	public Rigidbody2D GetBody () {
+		return body;
+	}
+	
+	public bool IsGrounded() {
+		Vector2 lineEnd = new Vector2( boxCollider.bounds.center.x, boxCollider.bounds.min.y - GROUND_RAY_LENGTH );
+		return Physics2D.Linecast( boxCollider.bounds.center, lineEnd, groundLayer );
+	}
+
+	void FixedUpdate () {
+		float xVelocity = Mathf.Lerp( body.velocity.x, TOP_SPEED * Input.GetAxisRaw( "Horizontal" ), SPEED_DAMPING );
+		body.velocity = new Vector2( xVelocity, body.velocity.y );
+
+		if( Input.GetButton( "Action" ) ) {
+			Actions.ExecuteCurrentAction( this );
+		}
+	}
+}
